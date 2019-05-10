5133790351d04bc1d35d0f72d533673b267b1c44
ItsCDubb <chris.warren.2019@gmail.com>
2019-05-09 19:55:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.guessinggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
